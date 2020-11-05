e3a4ef724b7f502c97a406221838831eeb187a1c
Boris Nimcevic <borisnimcevic@gmail.com>
2020-11-05 22:09:08 +0100

    Some sighns of life from tests

---
+++ test/main/main_test.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include "unity.h"
+
+
+void app_main()
+{
+    printf("testing kojesta\n");
+    UNITY_BEGIN();
+    unity_run_test_by_name("Standard case");
+    UNITY_END();
+
+    printf("running by tag\n");
+    UNITY_BEGIN();
+    unity_run_tests_by_tag("[double]", true);
+    UNITY_END();
+}
